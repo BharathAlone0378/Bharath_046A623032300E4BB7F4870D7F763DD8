2defc4705391f7c557ba050b3671d5f120bde7c7
BharathAlone0378 <144440822+BharathAlone0378@users.noreply.github.com>
2023-10-03 14:47:02 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,51 +1,28 @@
-'''Implement a class called BankAccount that represents a bank account. The class should have private 
-attributes for account number, account holder name, and account balance. Include methods to 
-deposit money, withdraw money, and display the account balance. Ensure that the account balance
-cannot be accessed directly from outside the class. Write a program to create an instance of the
-BankAccount class and test the deposit and withdrawal functionality.'''
+# 1.1 implement a recursive function to calculate the factorial of a given number.
+"""
+1!=1x1
+2!=2x1---->>2x1
+3!=3x2---->>3x2x1
+.
+.
+.
+.
+.
+.
+10!=10x9!--->>10x9x8x...x1
+
+formula - n x (n-1)!
+"""
 
 
-class BankAccount:
-  
-  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
-    self.__account_number = account_number
-    self.__account_holder_name = account_holder_name
-    self.__account_balance = initial_balance
-
-  def deposit(self, amount):
-    if amount > 0:
-      self.__account_balance += amount
-      # self.__account_balance = self.__account_balance+amount
-      print("Deposited ₹{}. New balance: ₹{}". format(account, self.__account_balance))
-    
-    else:
-      print("Invalid deposit amount.")
-      
-  def withdraw(self, amount):
-   if amount > 0 and amount<= self.__account_balance:
-    self.__account_balance-= amount 
-    # self.__account_balance = self.__account_balance - amount
-    print("withdrew ₹{}. New balance: ₹{}".format(amount,
-                                                  self.__account_balance))
-   else:
-     print("Invalid withdrawal amount or insufficient balance.") 
-     
-     def display_balance(self):
-       print ("Account balance for {} (Account #{}): ₹{}".format(
-         self.__account_holder_name, self.__account_number,
-         self.__account_balance))
+def fact_rec(n):
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n * fact_rec(n - 1)
 
 
-# Create an instance of the BankAccount class
-account = BankAccount(account_number="123456789",
-                      account_holder_name="Bharath",
-                            initial_balance=5000.0)
+number = int(input("Enter a value:"))
+res = fact_rec(number)
 
-# Test Deposit and withdrawal functionlity
-#account.display_balance()
-#account.Deposit(500.0)
-account.withdraw(200.0)
-account.withdraw(20000.0)
-#account.display_balance()
-
-  
+print("The Factorial of {} is {}.".format(number, res))
